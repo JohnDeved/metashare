46544e39edff34f141d01320e4670c8f2905bdc6
Johann <johann@objekt.stream>
2021-09-27 20:03:49 +0200

    + settings mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import logo from './logo.svg'
 import { useMetashare, usePostMeta } from './hooks/metashare'
 import { Helmet } from 'react-helmet'
-import { Text, Box, Button, Container, SimpleGrid, useColorMode, Image, Center, Heading, Flex } from '@chakra-ui/react'
+import { Text, Box, Button, Container, SimpleGrid, useColorMode, Image, Center, Heading, Flex, Stack, Input, InputGroup, InputLeftElement, Modal, ModalOverlay, useDisclosure, FormControl, FormLabel, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, HStack, Textarea, VStack } from '@chakra-ui/react'
+import { AddIcon, PhoneIcon, QuestionIcon, SearchIcon, SettingsIcon } from '@chakra-ui/icons'
 
 const PostCard: React.FC<{ id: string }> = ({ id }) => {
   const { loading, value } = usePostMeta(id)
@@ -17,6 +17,7 @@ const PostCard: React.FC<{ id: string }> = ({ id }) => {
 
 const App = () => {
   const { posts, peerCount, postCount, sendAll } = useMetashare()
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
     <Box >
@@ -25,12 +26,77 @@ const App = () => {
       </Helmet>
       <Box py={5}>
         <Container maxW="container.lg">
-          <Flex justifyContent="space-between" alignItems="center">
+          <Flex justifyContent="space-between" alignItems="center" wrap="wrap">
             <Box>
               <Heading size="lg">MetaShare</Heading>
               <Text>Seeders: {peerCount} • Posts: {postCount}</Text>
             </Box>
-            <Button>Submit Link</Button>
+            <Stack direction={['column', 'row']} align="stretch" >
+              <InputGroup>
+                <InputLeftElement pointerEvents="none"><SearchIcon/></InputLeftElement>
+                <Input variant="filled" placeholder="Search"/>
+              </InputGroup>
+              <Box>
+                <Button><AddIcon mr={2}/> Submit</Button>
+              </Box>
+              <Box>
+                <Button onClick={onOpen}><SettingsIcon/></Button>
+                <Modal
+                  isOpen={isOpen}
+                  onClose={onClose}
+                >
+                  <ModalOverlay/>
+                  <ModalContent>
+                    <ModalHeader>Profile Settings</ModalHeader>
+                    <ModalCloseButton/>
+                    <ModalBody pb={6}>
+                      <VStack spacing={4}>
+                        <FormControl>
+                          <FormLabel>
+                            <HStack>
+                              <Text>Username</Text>
+                              <Popover>
+                                <PopoverTrigger>
+                                  <QuestionIcon cursor="pointer" boxSize={3} color="whiteAlpha.500"/>
+                                </PopoverTrigger>
+                                <PopoverContent bg="gray.800">
+                                  <PopoverArrow bg="gray.800"/>
+                                  <PopoverBody>This will be displayed as credit on submission</PopoverBody>
+                                </PopoverContent>
+                              </Popover>
+                            </HStack>
+                          </FormLabel>
+                          <Input placeholder="Anonymous"/>
+                        </FormControl>
+                        <FormControl>
+                          <FormLabel>
+                            <HStack>
+                              <Text>Torrent Trackers</Text>
+                              <Popover>
+                                <PopoverTrigger>
+                                  <QuestionIcon cursor="pointer" boxSize={3} color="whiteAlpha.500"/>
+                                </PopoverTrigger>
+                                <PopoverContent bg="gray.800">
+                                  <PopoverArrow bg="gray.800"/>
+                                  <PopoverBody>You can add more Websocket Torrent Trackers if you feel like it. (Seperate Links with newlines)</PopoverBody>
+                                </PopoverContent>
+                              </Popover>
+                            </HStack>
+                          </FormLabel>
+                          <Textarea placeholder="wss://tracker.openwebtorrent.com/"/>
+                        </FormControl>
+                      </VStack>
+                    </ModalBody>
+                    <ModalFooter>
+                      <Button colorScheme="blue" mr={3}>
+                        Save
+                      </Button>
+                      <Button onClick={onClose}>Cancel</Button>
+                    </ModalFooter>
+                  </ModalContent>
+                </Modal>
+              </Box>
+            </Stack>
           </Flex>
         </Container>
       </Box>
